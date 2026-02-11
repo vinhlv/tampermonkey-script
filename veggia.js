f342cc8b24318d65d13f725e75a126f5f00e3ce0
er-vinhlv <vinhlv@tech.est-rouge.com>
2026-02-11 21:14:38 +0700

    a

--- veggia.js
+++ veggia.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         Auto Veggia
 // @namespace    http://tampermonkey.net/
-// @version      1.2
+// @version      1.3
 // @updateURL    https://raw.githubusercontent.com/vinhlv/tampermonkey-script/refs/heads/main/veggia.js
 // @downloadURL  https://raw.githubusercontent.com/vinhlv/tampermonkey-script/refs/heads/main/veggia.js
 // @description  try to take over the world!
@@ -102,7 +102,7 @@
           btn.addEventListener("click", () => {
             setTimeout(() => startClick(), 2000);
             setTimeout(() => startClick2(), 3000);
-            setTimeout(() => startAutoClick(), 20000);
+            setTimeout(() => startAutoClick(), 15000);
           });
 
           // auto mode
